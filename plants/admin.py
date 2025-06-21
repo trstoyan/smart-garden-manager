4a2cba6a588a2c21be29b3814dcd56f1d629f48d
trstoyan <stoyantr@icloud.com>
2025-06-21 16:09:36 +0300

    Register models in `plants/admin.py` for admin interface access

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Garden, PlantType, PlantGroup, Plant, CalendarEvent, Device, SensorReading, Notification
 
 # Register your models here.
+admin.site.register(Garden)
+admin.site.register(PlantType)
+admin.site.register(PlantGroup)
+admin.site.register(Plant)
+admin.site.register(CalendarEvent)
+admin.site.register(Device)
+admin.site.register(SensorReading)
+admin.site.register(Notification)
